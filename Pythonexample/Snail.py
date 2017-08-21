fe1d89f2385e4205d0bc061360c8d21c932b7aa0
inhome <lim01000@naver.com>
2017-08-21 16:41:10 +0900

    8/21 16:41

--- Pythonexample/Snail.py
+++ Pythonexample/Snail.py
@@ -7,6 +7,7 @@ for i in range(0, RowNum):
         MatrixColumn.append(j)
     MatrixRow.append(MatrixColumn)
     MatrixColumn = []
+print(MatrixRow)
 LineIndex = 0
 j = 1
 Max = ColumnNum *  RowNum
@@ -29,11 +30,16 @@ while 1:
     for i in range(LineIndex+1, RowNum-LineIndex):
         MatrixRow[RowNum-i][LineIndex] = j
         j = j + 1
-        if j > Max:
+        if j >= Max:
             break
     LineIndex = LineIndex + 1
+    if ColumnNum%2 == 1 and ColumnNum == RowNum:
+        MatrixRow[(RowNum//2)][(ColumnNum//2)]=Max
+        if j == Max:
+            break
     if j > Max:
         break
 for i in range(0, RowNum):
-    print(MatrixRow[i])
-print(MatrixRow)
+    for j in range (0, ColumnNum):
+        print(MatrixRow[i][j], end=" ")
+    print(" ")
--- Pythonexample/test.py
+++ Pythonexample/test.py
@@ -1,3 +1,10 @@
-a = input()
-print(a)
-print(int(a))
+f = open("파일입출력연습.txt", 'w')
+for i in range(1,11):
+    data = "%d번째 줄입니다.\n" %i
+    f.write(data)
+f.close()
+
+f = open("파일입출력연습.txt", 'r')
+line = f.read()
+print(line)
+f.close()
