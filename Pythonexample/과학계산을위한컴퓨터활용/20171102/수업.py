863fa9d4f0c6fb4267672c9c76c3b8a413b6d3a7
inhome <lim01000@naver.com>
2017-11-09 11:20:03 +0900

    2017/11/09 11:19 LG_Gram

--- Pythonexample/과학계산을위한컴퓨터활용/20171102/수업.py
+++ Pythonexample/과학계산을위한컴퓨터활용/20171102/수업.py
@@ -1,3 +1,4 @@
+'''
 m = 'hello'
 a = 12
 x = 123.123456789
@@ -13,3 +14,75 @@ print("Message = {:>10s}, iter = {:10d}, x = {:20.3f}, y = {:20.3e}".format(m,a,
 print("Message = {:^10s}, iter = {:10d}, x = {:20.3f}, y = {:20.3e}".format(m,a,x,y))
 print("Message = {:<10s}, iter = {:010d}, x = {:20.3g}, y = {:20.3g}".format(m,a,x,y))
 print("x = {2:20.3g}, y = {3:20.3g}, Message = {0:^10s}, iter = {1:10d}".format(m,a,x,y))
+'''
+
+'''
+def myadd(a, b):
+    c = a + b
+    return c
+
+def mymul(a, b):
+    c = a * b
+    return c
+
+def print_val(f, a, b):
+    print(f(a, b))
+
+def myaddmul(a,b):
+    c = a + b
+    d = a * b
+    return c, d
+
+def myadd2(a, b):
+    c = a + b
+    m ='addition'
+    return c, m
+
+vala = int(input())
+valb = int(input())
+print("myadd = %d" %myadd(vala, valb))
+print("print_val(myadd) = ", end="")
+print_val(myadd, vala, valb)
+print("print_val(mynul) = ", end="")
+print_val(mymul, vala, valb)
+a, b = myaddmul(vala, valb)
+print("myaddmul = ", end = "")
+print((a, b))
+print("myaddmul = ", end = "")
+print(myaddmul(vala,valb))
+print("myadd2 = ", end = "")
+print(myadd2(vala, valb))
+print("myadd2 = {}".format(myadd2(vala, valb)))
+
+#import는 한번 하고 다시 하지않음
+#따라서 현재를 리셋하고 다시 import해야 한다
+'''
+#bisection
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return x ** 3 + 4 * x ** 2 - 10
+while 1:
+    a = int(input("a = "))
+    b = int(input("b = "))
+    if a > b:
+        temp = a
+        a = b
+        b = temp
+    elif a == b:
+        print('a cannot equal b')
+    if f(a) * f(b) > 0:
+        print("f(a) and f(b) have same sign")
+    else:
+        break
+while 1:
+    TOLinte = int(input("TOL 10^-() = "))
+    TOL = 10 ** (-TOLinte)
+    if TOL <= 0:
+        print('Tolerance must be positive')
+    else:
+        break
+N0 = 100
+x = np.linspace(a, b, 100)
+y = f(x)
